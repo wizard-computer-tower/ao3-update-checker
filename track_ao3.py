bd595295097d2db1ba94634cfd233b9da57fabe4
wizard-computer-tower <92277421+wizard-computer-tower@users.noreply.github.com>
2021-10-11 21:05:40 -0400

    Output now easier to edit with web_template.html
    Fixed bug from last commit
    
    Bug fix
    
    Fixed template.html

--- track_ao3.py
+++ track_ao3.py
@@ -3,10 +3,10 @@ import datetime as dt
 import webbrowser
 
 def has_update(work,last_checked):
-    ## Returns True if work has updated since last_checked
-    ## AO3.Work -> Bool
-    last_edited = work.date_updated # request from AO3 (1/ work)
-    return last_edited>=last_checked # Bigger than means after
+    ## Returns True if work has updated on or since last_checked
+    ## AO3.Work Datetime -> Bool
+    last_updated = work.date_updated # request from AO3 (1/ work)
+    return last_updated>=last_checked # Bigger than means after
 
 def is_incomplete(work):
     ## Returns True if work is incomplete
@@ -14,9 +14,10 @@ def is_incomplete(work):
     return work.status == "Work in Progress"
 
 def get_objects_from_file(file_name,constructor):
-    ## Returns a list of objects made with constructor from file_name
-    ## Requires: constructor takes one input
-    ## Str Func -> (listof AO3.User)
+    ## Returns a list of objects made with constructor from file_name,
+    ##  or empty list if file_name does not exist
+    ## Requires: constructor takes only one input
+    ## Str Func -> (listof Any)
     try:
         with open(file_name) as a:
             return list(map(constructor, a.readlines()))
@@ -32,7 +33,7 @@ def remove_completed_works(work_file_name,tracked_works):
         f.writelines(list(map(lambda w: w.id, still_incomplete)))
 
 def get_all_works_from_multiple_authors(listof_authors):
-    ## Returns a list of all works by all authors in the list of authors
+    ## Returns a list of all works by all authors in listof_authors
     ## (listof AO3.User) -> (listof AO3.Work)
     works_by_authors = []
     for author in listof_authors:
@@ -40,9 +41,10 @@ def get_all_works_from_multiple_authors(listof_authors):
     return works_by_authors
 
 def get_all_updated_works(author_objects,work_objects,date_last_checked):
-    ## Returns a list of all works by all authors in 
-    ##   the files represented by author_file_name and work_file_name
-    ## Str Str Datetime -> (listof AO3.Work)
+    ## Returns a list of all works by all authors in author_objects and 
+    ##   all works in work_objects that have been published or updated 
+    ##   on or after date_last_checked
+    ## (listof AO3.User) (listof AO3.Work) Datetime -> (listof AO3.Work)
     work_objects_by_authors = get_all_works_from_multiple_authors(author_objects)
 
     all_work_objects = []
@@ -53,35 +55,36 @@ def get_all_updated_works(author_objects,work_objects,date_last_checked):
 
     return updated_work_objects
 
-def repr_work_as_li_element(work,template):
-    ## Returns a representation of a given work as an HTML li element
+def repr_work_from_template(work,template):
+    ## Returns a representation of work according to template
+    ## Requires: template contains fields for url, title, and authors of work, and no more
     ## AO3.Work Str -> Str
-    #work = work.reload()
     url = work.url
     title = work.title
     authors = list(map(lambda a: a.username, work.authors))
     authors_str = ", ".join(authors)
     return template.format(url=url,title=title,authors=authors_str)
 
-def make_html(works,element_template,head_template,output_file_name,since_date):
+def make_html(works,element_template,web_template,output_file_name,since_date):
     ## Creates an HTML file named output_file_name listing each item in works as 
-    ##   an element in element_template. The file begins with head_template and lists
-    ##   since_date on the page
-    ## (listof AO3.Work) Str Str -> None
-    with open(head_template) as t:
-        head = t.readline()
+    ##   an element like in the file named element_template. The file is made
+    ##   according to web_template and contains since_date
+    ## (listof AO3.Work) Str Str Datetime -> None
+    with open(web_template) as t:
+        web_template = "\n".join(t.readlines())
 
     with open(element_template) as t:
-        element = t.readline()
+        element = "\n".join(t.readlines())
 
-    elements = list(map(lambda w: repr_work_as_li_element(w,element),works))
+    elements = list(map(lambda w: repr_work_from_template(w,element),works))
 
     body_date = "<time datetime="+str(since_date)+">"+str(since_date)+"</time>"
-    body_start = "<body><h1>Updated since {time_element}</h1><ul>".format(time_element=body_date)
-    body = body_start+"".join(elements)+"</ul></body></html>"
+    body_elements = "".join(elements)
+
+    web_page = web_template.format(elements=body_elements,datetime=body_date)
 
     with open(output_file_name,"w") as f:
-        f.write(head+body)
+        f.write(web_page)
 
 if __name__ == "__main__":
     date_format = "%Y-%m-%d"
@@ -101,9 +104,9 @@ if __name__ == "__main__":
     works_list = get_all_updated_works(author_objects_global,work_objects_global,date)
 
     file_name_element = "resource/element.html"
-    file_name_head = "resource/head.html"
+    file_name_web = "resource/web_template.html"
     file_name_output = "updates.html"
-    make_html(works_list,file_name_element,file_name_head,file_name_output,date)
+    make_html(works_list,file_name_element,file_name_web,file_name_output,date)
 
     with open(last_checked_file_name,"w") as f:
         f.write(str(dt.datetime.now())[:10])
